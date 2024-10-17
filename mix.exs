94197c516489807d77837c5b0c67b3b737256e33
dlustre <dennis.lustre@gmail.com>
2024-10-17 15:18:20 -0700

    print decoded json

--- mix.exs
+++ mix.exs
@@ -9,13 +9,13 @@ defmodule App.MixProject do
       escript: [main_module: Bittorrent.CLI],
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      deps: deps()
     ]
   end
-  
+
   defp deps do
     [
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.2"}
     ]
   end
 end
