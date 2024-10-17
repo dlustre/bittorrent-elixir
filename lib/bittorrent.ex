69049ad2672ee0330f21c73a035236bfe48da0af
dlustre <dennis.lustre@gmail.com>
2024-10-17 16:07:33 -0700

    decode bencoded integers

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -20,15 +20,25 @@ defmodule Bencode do
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
 
-    case Enum.find_index(binary_data, fn char -> char == 58 end) do
-      nil ->
-        IO.puts("The ':' character is not found in the binary")
+    case binary_data do
+      [?i | _] = int ->
+        parse_int(int)
 
-      index ->
-        rest = Enum.slice(binary_data, (index + 1)..-1)
-        List.to_string(rest)
+      _ ->
+        case Enum.find_index(binary_data, fn char -> char == ?: end) do
+          nil ->
+            IO.puts("The ':' character is not found in the binary")
+
+          index ->
+            rest = Enum.slice(binary_data, (index + 1)..-1)
+            List.to_string(rest)
+        end
     end
   end
 
   def decode(_), do: "Invalid encoded value: not binary"
+
+  def parse_int([?i | rest]), do: parse_int(rest, [])
+  def parse_int([?e], acc), do: acc |> Enum.reverse() |> List.to_integer()
+  def parse_int([character | rest], acc), do: parse_int(rest, [character | acc])
 end
