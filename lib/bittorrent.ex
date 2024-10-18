d2eb20ab8fa1677e3bf9926b81e4f6f5c9d060d6
dlustre <dennis.lustre@gmail.com>
2024-10-17 19:03:51 -0700

    decode bencoded lists

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -20,25 +20,47 @@ defmodule Bencode do
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
 
-    case binary_data do
-      [?i | _] = int ->
-        parse_int(int)
-
-      _ ->
-        case Enum.find_index(binary_data, fn char -> char == ?: end) do
-          nil ->
-            IO.puts("The ':' character is not found in the binary")
-
-          index ->
-            rest = Enum.slice(binary_data, (index + 1)..-1)
-            List.to_string(rest)
-        end
-    end
+    {_, result} = parse(binary_data)
+    result
   end
 
   def decode(_), do: "Invalid encoded value: not binary"
 
+  def parse([?l | _] = list), do: parse_list(list)
+  def parse([?i | _] = int), do: parse_int(int)
+  def parse(string), do: parse_string(string, [])
+
+  def parse_string([?: | rest], acc),
+    do: parse_string_value(rest, acc |> Enum.reverse() |> List.to_integer(), [])
+
+  def parse_string([character | rest], acc), do: parse_string(rest, [character | acc])
+
+  def parse_string_value(content, 0, acc),
+    do: {content, acc |> Enum.reverse() |> List.to_string()}
+
+  def parse_string_value([character | rest], length, acc),
+    do: parse_string_value(rest, length - 1, [character | acc])
+
+  # def parse_string(string) do
+  #   case Enum.find_index(string, fn char -> char == ?: end) do
+  #     nil ->
+  #       IO.puts("The ':' character is not found in the binary")
+
+  #     index ->
+  #       rest = Enum.slice(string, (index + 1)..-1)
+  #       List.to_string(rest)
+  #   end
+  # end
+
   def parse_int([?i | rest]), do: parse_int(rest, [])
-  def parse_int([?e], acc), do: acc |> Enum.reverse() |> List.to_integer()
+  def parse_int([?e | rest], acc), do: {rest, acc |> Enum.reverse() |> List.to_integer()}
   def parse_int([character | rest], acc), do: parse_int(rest, [character | acc])
+
+  def parse_list([?l | rest]), do: parse_list(rest, [])
+  def parse_list([?e | rest], acc), do: {rest, acc |> Enum.reverse()}
+
+  def parse_list(list, acc) do
+    {rest, result} = parse(list)
+    parse_list(rest, [result | acc])
+  end
 end
